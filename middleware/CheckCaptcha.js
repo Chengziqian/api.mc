2fa78390c6157578d0e382835167abc02def11e6
Chengziqian <18663104463@163.com>
2018-09-01 22:13:26 +0800

    captcha no-up-lower

--- middleware/CheckCaptcha.js
+++ middleware/CheckCaptcha.js
@@ -2,7 +2,7 @@ const DB = require('../libs/DB_Service');
 const createError = require('http-errors');
 const moment = require('moment');
 module.exports = function (httpReq, httpRes, next) {
-  let code = httpReq.body.captcha;
+  let code = httpReq.body.captcha.toLowerCase();
   let token = httpReq.headers['captcha-token'] || httpReq.headers['Captcha-Token'];
   DB.GET('captcha', 'captcha_token', token).then(r => {
     if (r.length === 0) return Promise.reject(createError(422, {message: {captcha: ['验证码错误']}}));
