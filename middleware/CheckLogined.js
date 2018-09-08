428ba424f6b6575649594b156d4788d1a6bed578
Chengziqian <18663104463@163.com>
2018-09-08 18:30:38 +0800

    login check

--- middleware/CheckLogined.js
+++ middleware/CheckLogined.js
@@ -11,29 +11,33 @@ let getClientIp = function (req) {
 
 module.exports = function (httpReq, httpRes, next) {
   let token = httpReq.headers['api-token'] || httpReq.headers['Api-Token'];
-  let api_token = {};
-  DB.GET('api_token', 'token', token, 'first').then(res => {
-    if (res.length === 0) return Promise.reject(createError(401, {message:'请先登录'}));
-    else {
-      api_token = res[0];
-      old_token = api_token.token;
-      if ((api_token.expired_time === null || moment(api_token.expired_time).isAfter(moment())) &&
-        api_token.ip === getClientIp(httpReq)) {
-        return DB.GET('users', 'id', res[0].user_id);
-      } else {
-        return Promise.reject(createError(401, {message:'请先登录'}));
+  if (!token) {
+    next(createError(401, {message:'请先登录'}))
+  } else {
+    let api_token = {};
+    DB.GET('api_token', 'token', token, 'first').then(res => {
+      if (res.length === 0) return Promise.reject(createError(401, {message:'请先登录'}));
+      else {
+        api_token = res[0];
+        old_token = api_token.token;
+        if ((api_token.expired_time === null || moment(api_token.expired_time).isAfter(moment())) &&
+          api_token.ip === getClientIp(httpReq)) {
+          return DB.GET('users', 'id', res[0].user_id);
+        } else {
+          return Promise.reject(createError(401, {message:'请先登录'}));
+        }
       }
-    }
-  }).then(res => {
-    if (res.length === 0) return Promise.reject(createError(401, {message:'请先登录'}));
-    else {
-      httpReq.USER = res[0];
-      return Promise.reject('next');
-    }
-  }).catch(e => {
-    if (e === 'next') next();
-    else {
-      next(e);
-    }
-  })
+    }).then(res => {
+      if (res.length === 0) return Promise.reject(createError(401, {message:'请先登录'}));
+      else {
+        httpReq.USER = res[0];
+        return Promise.reject('next');
+      }
+    }).catch(e => {
+      if (e === 'next') next();
+      else {
+        next(e);
+      }
+    })
+  }
 };
